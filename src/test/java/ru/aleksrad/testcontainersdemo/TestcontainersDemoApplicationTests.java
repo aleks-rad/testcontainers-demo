6bec643f6d4112dadab2541ebd4879efc3dc527c
Alexey Radionenko <radionenko.alexey@gmail.com>
2022-08-29 19:07:13 +0300

    Initial spring app

---
+++ src/test/java/ru/aleksrad/testcontainersdemo/TestcontainersDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aleksrad.testcontainersdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainersDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
